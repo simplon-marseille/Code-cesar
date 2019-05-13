6c05c76f5cadb761c71d77d24fdd4d02423c9414
simplon-marseille <acardnicolas@hotmail.com>
2019-05-13 16:26:36 +0200

    first commit

---
+++ code-césar.js
@@ -0,0 +1,17 @@
+//Code césar - cryptographie
+//créez une fonction qui prend en paramètre une chaine de caractère.
+//Cette fonction doit renvoyer une chaine cryptée.
+//Crytpage : décaler chaque lettre un cran vers la droite.
+//
+//ex: chaine non cryptée : simplon
+//    chaine cryptée : tjnqmpo
+//
+//    Vous devez crypter dataNonCrypte1 et dataNonCrypte2
+//courage ! :)
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const dataNonCrypte1 = "simplon";
+const dataNonCrypte2 = "zoo"; //la lettre 'z' deviendra un 'a';
+
+const dataCrypté = (data) => {
+  //TODO
+};
